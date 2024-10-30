76fa740811a60533d49e044b12ad4d793819445d
BunedLutfulloev <blutfulloyev@bk.ru>
2024-10-30 19:51:27 +0400

    fix: simplified auth requirements

--- src/pages/login/lib/useFormManagement.ts
+++ src/pages/login/lib/useFormManagement.ts
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import { setAuth } from "../../../redux/slices/authSlice";
-import { EMAIL_REGEX, PASSWORD_REGEX } from "../../../utils/regex";
 import { useAppDispatch } from "./../../../hooks/redux-hooks";
 
 export const useFormManagement = () => {
@@ -11,18 +10,8 @@ export const useFormManagement = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const validationSchema = yup.object({
-        email: yup
-            .string()
-            .email("Enter a valid email")
-            .required("Email is required")
-            .matches(EMAIL_REGEX, "Invalid email format"),
-        password: yup
-            .string()
-            .min(8, "Password should be of minimum 8 characters length")
-            .max(30, "Password should be of maximum 30 characters length")
-            .matches(/^[A-Z]/, "Password must start with a capital letter") // Проверка на первый символ - буква
-            .matches(/\d/, "Password must contain at least one digit") // Проверка на наличие хотя бы одной цифры
-            .required("Password is required"),
+        email: yup.string().required("Email is required").equals(["admin"], "Invalid email"),
+        password: yup.string().required("Password is required").equals(["admin"], "Invalid password"),
     });
 
     const formik = useFormik({
@@ -32,8 +21,8 @@ export const useFormManagement = () => {
         },
         validationSchema: validationSchema,
         onSubmit: (values) => {
-            const isEmailValid = EMAIL_REGEX.test(values.email);
-            const isPasswordValid = PASSWORD_REGEX.test(values.password);
+            const isEmailValid = values.email === "admin";
+            const isPasswordValid = values.password === "admin";
             setIsLoading(true);
 
             if (isEmailValid && isPasswordValid) {
